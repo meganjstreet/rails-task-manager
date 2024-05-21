42e2010560a34d224e46481df6920dcc7f59612b
Megan Street <meganjstreet@gmail.com>
2024-05-21 14:41:00 +0200

    Creating todo list on rails

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,13 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  get "tasks", to: "tasks#index"
+  get "new", to: "tasks#new"
+  post "tasks", to: "tasks#create"
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit
+  patch "tasks/:id", to: "tasks#update"
+  get "tasks/:id", to: "tasks#show", as: :task
+  delete "tasks/:id", to: "tasks#destroy"
+
+
 end
